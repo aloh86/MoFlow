a55b0499f300ac6757ce2f30c97caac7b3ff868f
Alex Oh <alexoh86@gmail.com>
2014-12-06 00:08:31 -0800

    Code cleanup for GroupListActivity
    Cleaned up the code, mostly removing spaces within parentheses and brace
    changes. Style change was made for better readability using K&R style.

--- src/moflow/activities/GroupListActivity.java
+++ src/moflow/activities/GroupListActivity.java
@@ -5,7 +5,6 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.util.SparseBooleanArray;
 import android.view.*;
-
 import android.os.Bundle;
 import android.widget.*;
 import moflow.dialogs.NameDialogFragment;
@@ -13,19 +12,15 @@ import moflow.dialogs.SimpleDialogListener;
 import moflow.utility.Key;
 import moflow.utility.DBTransaction;
 import moflow.utility.NameModifier;
-import moflow.wolfpup.Creature;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
-/*
-===============================================================================
-GroupListActivity.java
-
-Activity for listing the existing parties/encounters. The user can create a new
-party/encounter and edit or delete an existing party/encounter.
-===============================================================================
-*/
+/**
+ * GroupListActivity.java
+ *
+ * Activity for listing the existing parties/encounters. The user can create a new
+ * party/encounter and edit or delete an existing party/encounter.
+ */
 public class GroupListActivity extends ListActivity implements AdapterView.OnItemClickListener, SimpleDialogListener, AbsListView.MultiChoiceModeListener
 {
     private DBTransaction dbTransaction;
@@ -40,31 +35,30 @@ public class GroupListActivity extends ListActivity implements AdapterView.OnIte
     private String groupType;
 
 	@Override
-    public void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
         try {
-            groupType = getIntent().getExtras().getString( Key.GROUP_TYPE);
-        } catch ( NullPointerException npe ) {
-            Toast.makeText( this, "onCreate: groupType Extra could not be found.", Toast.LENGTH_LONG );
+            groupType = getIntent().getExtras().getString(Key.GROUP_TYPE);
+        } catch (NullPointerException npe) {
+            Toast.makeText(this, "onCreate: groupType Extra could not be found.", Toast.LENGTH_LONG);
+            finish();
         }
 
-        this.setTitle( groupType.equals( Key.Val.PARTY) ? "Parties" : "Encounters" );
+        this.setTitle(groupType.equals( Key.Val.PARTY) ? "Parties" : "Encounters");
 
-        dbTransaction = new DBTransaction( this );
+        dbTransaction = new DBTransaction(this);
 
-        groupList = dbTransaction.getGroupList( groupType );
+        groupList = dbTransaction.getGroupList(groupType);
 
         // fill the list with parties from database
-        listAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                groupList);
-        setListAdapter( listAdapter );
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, groupList);
+        setListAdapter(listAdapter);
 
         getListView().setOnItemClickListener(this);
-        getListView().setChoiceMode( ListView.CHOICE_MODE_MULTIPLE_MODAL );
-        getListView().setMultiChoiceModeListener( this );
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+        getListView().setMultiChoiceModeListener(this);
   
         editMode = false;
         deleteMode = false;
@@ -73,114 +67,119 @@ public class GroupListActivity extends ListActivity implements AdapterView.OnIte
 
         indexOfItemToEdit = -1;
 
-        renameDialog = new NameDialogFragment( "Rename" );
-        newGroupDialog = new NameDialogFragment( "Name" );
+        renameDialog = new NameDialogFragment("Rename");
+        newGroupDialog = new NameDialogFragment("Name");
     }
 
     /**
      * Creates action bar menu.
      */
     @Override
-    public boolean onCreateOptionsMenu( Menu menu ) {
-        // Inflate the menu items for use in the action bar
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate( R.menu.actionbar_add, menu );
-        return super.onCreateOptionsMenu( menu );
+        inflater.inflate(R.menu.actionbar_add, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     /**
      * Action bar item handling.
      */
     @Override
-    public boolean onOptionsItemSelected( MenuItem item ) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle presses on the action bar items
-        switch ( item.getItemId() ) {
+        switch (item.getItemId()) {
             case R.id.action_new:
                 newGroupDialog.show(getFragmentManager(), "newGroupDialog");
                 break;
             case R.id.action_help:
-                Toast.makeText( this, "Long-click to edit or delete an item. Tap an item to open.", Toast.LENGTH_LONG ).show();
+                Toast.makeText(this, "Long-click to edit or delete an item. Tap an item to open.", Toast.LENGTH_LONG )
+                        .show();
                 break;
             default:
-                return super.onOptionsItemSelected( item );
+                return super.onOptionsItemSelected(item);
         }
         return false;
     }
 
     /**
      * Handle list item clicks
-     * @param listView
-     * @param view
-     * @param position
-     * @param id
+     * @param listView The collection of items in the list.
+     * @param view The view in the list clicked on.
+     * @param position The index of the position clicked.
+     * @param id The row id of item clicked.
      */
     @Override
-    public void onItemClick( AdapterView<?> listView, View view, int position, long id ) {
-            Intent intent = new Intent( "moflow.activities.EditGroupActivity" );
-            intent.putExtra( Key.GROUP_TYPE, groupType );
-            intent.putExtra( Key.GROUP_NAME, listAdapter.getItem( position ) );
-            startActivityForResult( intent, 1 );
+    public void onItemClick(AdapterView<?> listView, View view, int position, long id)
+    {
+        Intent intent = new Intent("moflow.activities.EditGroupActivity");
+        intent.putExtra(Key.GROUP_TYPE, groupType);
+        intent.putExtra(Key.GROUP_NAME, listAdapter.getItem(position));
+        startActivityForResult(intent, 1);
     }
 
     /**
      * Handler for positive dialog click
-     * @param dialog
+     * @param dialog The dialog fragment handling the positive click.
      */
     @Override
-    public void onDialogPositiveClick( DialogFragment dialog ) {
-        if ( dialog == renameDialog ) {
-            renameParty( dialog );
+    public void onDialogPositiveClick(DialogFragment dialog)
+    {
+        if (dialog == renameDialog) {
+            renameParty(dialog);
         }
-
-        else if ( dialog == newGroupDialog ) {
-            createNewGroup( dialog );
+        else if (dialog == newGroupDialog) {
+            createNewGroup(dialog);
         }
     }
 
     /**
      * Handler for negative dialog click
-     * @param dialog
+     * @param dialog The dialog fragment handling the negative click.
      */
     @Override
-    public void onDialogNegativeClick( DialogFragment dialog ) {
+    public void onDialogNegativeClick(DialogFragment dialog)
+    {
         indexOfItemToEdit = -1;
     }
 
     /**
      * Renames party and saves new name to database
-     * @param dialog
+     * @param dialog The dialog used to rename groups.
      */
-    private void renameParty( DialogFragment dialog ) {
-        EditText et = ( EditText ) dialog.getDialog().findViewById( R.id.nameField );
+    private void renameParty(DialogFragment dialog)
+    {
+        EditText et = (EditText) dialog.getDialog().findViewById(R.id.nameField);
         String uniqueName = et.getText().toString().trim();
-        String oldName = listAdapter.getItem( indexOfItemToEdit );
+        String oldName = listAdapter.getItem(indexOfItemToEdit);
 
-        if ( uniqueName.equals( oldName ) )
+        if (uniqueName.equals(oldName))
             return;
 
-        if ( !uniqueName.isEmpty() ) {
+        if (!uniqueName.isEmpty()) {
             uniqueName = NameModifier.makeNameUnique(groupList, uniqueName);
-
             dbTransaction.renameGroup(uniqueName, groupList.get(indexOfItemToEdit), groupType);
             groupList.set(indexOfItemToEdit, uniqueName);
-            Collections.sort( groupList );
+            Collections.sort(groupList);
             listAdapter.notifyDataSetChanged();
         }
     }
 
     /**
      * Creates new group and saves new party to database
-     * @param dialog
+     * @param dialog The dialog to create/name a new group.
      */
-    private void createNewGroup( DialogFragment dialog ) {
-        EditText et = ( EditText ) dialog.getDialog().findViewById( R.id.nameField );
+    private void createNewGroup(DialogFragment dialog)
+    {
+        EditText et = (EditText) dialog.getDialog().findViewById(R.id.nameField);
         String uniqueName = et.getText().toString().trim();
 
-        if ( !uniqueName.isEmpty() ) {
-            uniqueName = NameModifier.makeNameUnique(groupList, uniqueName );
-            dbTransaction.insertNewGroup( uniqueName, groupType );
-            groupList.add( uniqueName );
-            Collections.sort( groupList );
+        if (!uniqueName.isEmpty()) {
+            uniqueName = NameModifier.makeNameUnique(groupList, uniqueName);
+            dbTransaction.insertNewGroup(uniqueName, groupType);
+            groupList.add(uniqueName);
+            Collections.sort(groupList);
             listAdapter.notifyDataSetChanged();
         } else {
             Toast.makeText(this, groupType + " name required.", Toast.LENGTH_LONG).show();
@@ -188,19 +187,20 @@ public class GroupListActivity extends ListActivity implements AdapterView.OnIte
      }
 
     @Override
-    public void onItemCheckedStateChanged(ActionMode actionMode, int position, long id, boolean checked) {
-        if ( checked )
-            deleteList.add( listAdapter.getItem( position ) );
+    public void onItemCheckedStateChanged(ActionMode actionMode, int position, long id, boolean checked)
+    {
+        if (checked)
+            deleteList.add(listAdapter.getItem(position));
         else
-            deleteList.remove( listAdapter.getItem( position ) );
+            deleteList.remove(listAdapter.getItem(position));
 
-        MenuItem item = actionMode.getMenu().findItem( R.id.action_edit );
+        MenuItem item = actionMode.getMenu().findItem(R.id.action_edit);
 
-        if ( deleteList.size() > 1 ) {
+        if (deleteList.size() > 1) {
             indexOfItemToEdit = -1;
             item.setEnabled(false);
             item.getIcon().setAlpha(128);
-        } else if ( deleteList.size() == 1 ) {
+        } else if (deleteList.size() == 1) {
             SparseBooleanArray checkedItems = getListView().getCheckedItemPositions();
             for (int i = 0; i < checkedItems.size(); i++) {
                 if (checkedItems.valueAt(i)) {
@@ -216,35 +216,38 @@ public class GroupListActivity extends ListActivity implements AdapterView.OnIte
     }
 
     @Override
-    public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+    public boolean onCreateActionMode(ActionMode actionMode, Menu menu)
+    {
         listAdapter = null;
         listAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_checked,
                 groupList);
-        setListAdapter( listAdapter );
+        setListAdapter(listAdapter);
         MenuInflater inflater = actionMode.getMenuInflater();
-        inflater.inflate( R.menu.context_actionbar_delete, menu );
-        menu.findItem( R.id.action_edit ).setVisible( true );
+        inflater.inflate(R.menu.context_actionbar_delete, menu);
+        menu.findItem(R.id.action_edit).setVisible(true);
 
         return true;
     }
 
     @Override
-    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu)
+    {
         return false;
     }
 
     @Override
-    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-        switch ( menuItem.getItemId() ) {
+    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem)
+    {
+        switch (menuItem.getItemId()) {
             case R.id.action_discard:
                 deleteSelectedItems();
                 listAdapter.notifyDataSetChanged();
                 actionMode.finish();
                 return true;
             case R.id.action_edit:
-                renameDialog.show( getFragmentManager(), "renameDialog" );
+                renameDialog.show(getFragmentManager(), "renameDialog");
                 actionMode.finish();
                 return true;
         }
@@ -252,15 +255,17 @@ public class GroupListActivity extends ListActivity implements AdapterView.OnIte
     }
 
     @Override
-    public void onDestroyActionMode(ActionMode actionMode) {
+    public void onDestroyActionMode(ActionMode actionMode)
+    {
         deleteList.clear();
     }
 
-    private void deleteSelectedItems() {
+    private void deleteSelectedItems()
+    {
         dbTransaction.deleteGroup(deleteList, groupType);
 
-        for ( String s : deleteList ) {
-            listAdapter.remove( s );
+        for (String s : deleteList) {
+            listAdapter.remove(s);
         }
     }
 }
